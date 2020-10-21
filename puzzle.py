c1e92a9641087c3b3cde2bba82c8c351048d27cf
gractang <purple.grace.t@gmail.com>
2020-10-21 13:41:52 -0700

    added an ugly ass method that may or may not work; i haven't actually tested it yet

--- puzzle.py
+++ puzzle.py
@@ -1,14 +1,87 @@
 import sys
+import math
 
-def read_data(file_path):
+def DebugPrint(state):
+	n = int(math.sqrt(len(state)))
+	for row in range(n):
+		for col in range(n):
+			print(str(state[row*n+col]) + ' ', end='')
+		print()
+
+def LoadFromFile(file_path):
 	file = open(file_path, "r")
-	print(file.read())
-	
+	n = int(file.readline())
+	board = []
+	for i in range(n):
+		row = file.readline()
+		row = row.replace('\n', '')
+		row = row.replace('\t', ' ')
+		row = row.split()
+		int_row = []
+		for num in row:
+			if num == '*':
+				int_row.append(n*n)
+			else:
+				int_row.append(int(num))
+		board.extend(int_row)
+	return board
 
-def is_goal(state):
+def Build_Positions(state):
+	n = len(state)
+	adjacency_dict = {}
+	for index in range(n*n):	# for each index
+		adjacents = []			# build list of adjacent indexes
+		if index == 0: 			# upper left special case
+			adjacents.append(1)
+			adjacents.append(3)
+		elif index == n - 1: 		# upper right special case
+			adjacents.append(n - 2)
+			adjacents.append(2 * n - 1)
+		elif index == n * n - n:	# lower left special case
+			adjacents.append(n * n - 2 * n)
+			adjacents.append(n * n - n + 1)
+		elif index = n * n - 1:		# lower right special case
+			adjacents.append(n * n - 1 - n)
+			adjacents.append(n * n - 2)
+		else:
+			if index < n or index > n * n - n: # either first or last row
+				adjacents.append(index - 1)
+				adjacents.append(index + 1)
+				adjacents.append(index + n) if index < n else adjacents.append(index - n)
+			elif index % n == 0 or index % n == n - 1: # either first or last col
+				adjacents.append(index - n)
+				adjacents.append(index + n)
+				adjacents.append(index + 1) if index % n == 0 else adjacents.append(index - 1)
+			else:
+				adjacents.append(index - 1)
+				adjacents.append(index + 1)
+				adjacents.append(index + n)
+				adjacents.append(index - n)
+		adjacency_dict[index] = adjacents
+	return adjacency_dict
+
+
+
+
+def IsGoal(state):
+	for i in range(len(state)):
+		if i == 0:
+			continue
+		if state[i] < state[i-1]:
+			return False
 	return True
 
-def compute_neighbors(state):
+def find_neighbors_of_hole(state):
+	n = int(sqrt(len(state)))
+	index = -1
+	for i in range(len(state)):
+		if state[i] == n*n:
+			index = i
+			break
+
+
+def ComputeNeighbors(state):
+
 	return []
 
 def BFS(state):
@@ -18,15 +91,20 @@ def BFS(state):
 	while frontier:
 		current_state = frontier.pop(0)
 		discovered.add(current_state)
-	if is_goal(current_state):
+	if IsGoal(current_state):
 		# return the path you need by backtracking in parents
-	for neighbor in compute_neighbors(current_state):
-		if neighbor not in discovered:
-			frontier.append(neighbor)
-			discovered.add(neighbor)
-			# add neighbor: current_state to the parents map
+		for neighbor in ComputeNeighbors(current_state):
+			if neighbor not in discovered:
+				frontier.append(neighbor)
+				discovered.add(neighbor)
+				# add neighbor: current_state to the parents map
 
 if __name__ == '__main__':
-	filename = sys.argv[1]
-	print(filename)
-	read_data(file_path)
+	file_path = sys.argv[1]
+	print(file_path)
+	board = read_data(file_path)
+	print(board)
+	DebugPrint(board)
+	print(IsGoal(board))
+	print(IsGoal([7,8,2,1,4,3,5,0,6]))
+
